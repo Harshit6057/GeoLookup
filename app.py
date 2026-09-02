40ba7e3604c259b3bd73415a8cb9821f24b8389c
Harshit <harshitsb2615@gmail.com>
2026-09-02 16:39:20 +0530

    manifest.json solving the issue

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 name=str(input("Enter place:"))
 url = f"https://api.ambeedata.com/latest/by-city?city={name}"
 headers = {
-    "x-api-key": "3de0b3998988a16cbb1fbb9fdbcbeee0cc8e47d325c74c985ff14860247fbcfe"
+    "x-api-key": "XXXX"
 }
 
 response = requests.get(url, headers=headers)
